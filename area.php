b691137951eb0655c3e095d5d5fafef5ee062300
Bola Naguib <bolanaguib@gmail.com>
2019-04-04 01:12:48 +0200

    added aold ara page

--- area.php
+++ area.php
@@ -1,34 +1,13 @@
-<?php
-ob_start();
-include'header.php';
-
-require_once "connect/confi.php";
-require_once "connect/secri.php";
-$_GET['i'] = str_replace("'","",$_GET['i']);
-$_GET['n'] = str_replace("'","",$_GET['n']);
-
-if(empty($_GET['i']) || empty($_GET['n'])){
-    header("Location: index.php");
-}
-?>
+<?php include'header.php'; ?>
 <section>
   <div class="uk-text-center" uk-grid>
-  <?php
-//      echo myf2($_GET['n']);
-      $ms = mysqli_query($mu2,"SELECT * FROM areas WHERE section_id='".$_GET['i']."' ORDER BY id DESC");
-      while($ms2 = mysqli_fetch_assoc($ms)){
-          $name = $ms2['name'];
-          $id = $ms2['id'];
-      echo '  <div class="uk-width-1-3">
-      <a href="clients.php?n='.myf1($name).'&i='.myf1($id).'">
+    <div class="uk-width-1-3">
+      <a href="clients.php">
         <div class="uk-card uk-card-default uk-padding uk-padding-large">
-          <h1> '.myf2($name).' </h1>
+          <h1> مديرية الصحة </h1>
         </div>
       </a>
     </div>
-   ';
-      }
-      ?>
     <div class="uk-width-1-3">
       <a href="clients.php">
         <div class="uk-card uk-card-default uk-padding uk-padding-large">
@@ -59,17 +38,14 @@ if(empty($_GET['i']) || empty($_GET['n'])){
     </div>
 
     <div class="uk-width-1-3">
-      <a href="#sub_category" uk-toggle>
+      <a href="clients.php">
         <div class="uk-card uk-card-default uk-padding uk-padding-large">
           <h1>
             إضافة منطقة جديدة
           </h1>
         </div>
       </a>
-
     </div>
   </div>
 </section>
-<?php
-ob_end_flush();
-include'footer.php'; ?>
+<?php include'footer.php'; ?>
