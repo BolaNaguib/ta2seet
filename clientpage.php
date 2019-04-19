d5656f9e88c16e6f751d8a0c9f65ad300ea080c8
Bola Naguib <bolanaguib@gmail.com>
2019-04-19 02:24:56 +0200

    [ Added User Input Class]

--- clientpage.php
+++ clientpage.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 session_start();
 ob_start();
-include'header.php'; 
+include'header.php';
 
 require_once "connect/confi.php";
 require_once "connect/secri.php";
@@ -13,48 +13,48 @@ if(empty($_GET['i']) || empty($_SESSION['didi'])){
 $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
                $nuu=1;
                 $mmu = mysqli_fetch_assoc($mss);
-                   
+
                      $mss5 = mysqli_query($mu2,"SELECT * FROM areas WHERE id='".myf2($mmu['area'])."'");
              $m122mu = mysqli_fetch_assoc($mss5);
-       
+
             $mss35 = mysqli_query($mu2,"SELECT * FROM sections WHERE id='".myf2($mmu['section'])."'");
              $m1232mu = mysqli_fetch_assoc($mss35);
-                    
+
                      $s835 = mysqli_query($mu2,"SELECT * FROM el2st WHERE user_id='".myf1($mmu['id'])."'");
              $m89mu = mysqli_fetch_assoc($s835);
 }
-               
-                    
-             
+
+
+
 
 ?>
 <section>
   <h3>بيانات العميل</h3>
     <form method="post">
   <div class="uk-margin">
-    <b>الإسم :</b><span><input type="text" name="names" value="<?php echo myf2($mmu['name']); ?>"/></span>
+    <b>الإسم :</b><span><input class="userInput" type="text" name="names" value="<?php echo myf2($mmu['name']); ?>"/></span>
 
   </div>
   <div class="uk-margin">
-    <b>رقم التليفون :</b><span><input type="text" name="phone" value="<?php echo myf2($mmu['phone']); ?>"/></span>
+    <b>رقم التليفون :</b><span><input class="userInput" type="text" name="phone" value="<?php echo myf2($mmu['phone']); ?>"/></span>
 
   </div>
   <div class="uk-margin">
-    <b>العنوان :</b><span><input type="text" name="address" value="<?php echo myf2($mmu['address']); ?>"/></span>
+    <b>العنوان :</b><span><input class="userInput" type="text" name="address" value="<?php echo myf2($mmu['address']); ?>"/></span>
 
   </div>
   <div class="uk-margin">
-    <b>القسم :</b><span> <input type="text" name="section" value="<?php echo myf2($m1232mu['name']); ?>"/></span>
+    <b>القسم :</b><span> <input class="userInput" type="text" name="section" value="<?php echo myf2($m1232mu['name']); ?>"/></span>
 
   </div>
   <div class="uk-margin">
-    <b>المنطقة :</b><span> <input type="text" name="area" value="<?php echo myf2($m122mu['name']); ?>"/></span>
+    <b>المنطقة :</b><span> <input class="userInput" type="text" name="area" value="<?php echo myf2($m122mu['name']); ?>"/></span>
 
   </div>
   <div class="uk-margin">
-    <b>ميعاد القسط :</b><span> 
-            <?php 
-      
+    <b>ميعاد القسط :</b><span>
+            <?php
+
             $mi2is = mysqli_query($mu2,"SELECT * FROM el2st WHERE user_id='".$_GET['i']."'");
             $uus3w = mysqli_fetch_assoc($mi2is);
       $m2wi = mysqli_query($mu2,"SELECT * FROM pays WHERE client_id='".$_GET['i']."' AND el2st_or_cash='".myf1("2st")."' ORDER BY id DESC");
@@ -70,7 +70,7 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
       $calcutod=0;
               $nu=0;
                         //2019 : 03 : 02
-                     $texdate = myf2($ssww['date_today']); 
+                     $texdate = myf2($ssww['date_today']);
                 while($nu<strlen($texdate)){
                     if($nu<=3){
                       $date_sy .= $texdate[$nu];
@@ -84,11 +84,11 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
       $calcuold += $date_sy * 365;
       $calcuold += $date_sm * 30;
       $calcuold += $date_sd;
-     
+
       $calcutod += $date_yy * 365;
       $calcutod += $date_mm * 30;
       $calcutod += $date_dd;
-       
+
       $total = ($calcutod - $calcuold) - 30;
 
      if($total>0 && $calcuold>0){
@@ -97,12 +97,12 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
          if(!empty($uus3w['date_d']) && !empty($uus3w['date_m']) && !empty($uus3w['date_y'])){
               echo myf2($uus3w['date_d']) . " / " . myf2($uus3w['date_m']) . " / " . myf2($uus3w['date_y']);
          }
-          
-          
+
+
      }
 
-      
-      
+
+
       }else if(empty($ssww['id'])){
           $date_sd = "";
               $date_sm = "";
@@ -117,11 +117,11 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
       $calcuold += myf2($uus3w['date_y']) * 365;
       $calcuold += myf2($uus3w['date_m']) * 30;
       $calcuold += myf2($uus3w['date_d']);
-     
+
       $calcutod += $date_yy * 365;
       $calcutod += $date_mm * 30;
       $calcutod += $date_dd;
-       
+
       $total = ($calcutod - $calcuold) - 30;
 
      if($total>0 && $calcuold>0){
@@ -130,8 +130,8 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
          if(!empty($uus3w['date_d']) && !empty($uus3w['date_m']) && !empty($uus3w['date_y'])){
               echo myf2($uus3w['date_d']) . " / " . myf2($uus3w['date_m']) . " / " . myf2($uus3w['date_y']);
          }
-          
-          
+
+
      }
 
       }
@@ -157,21 +157,21 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
                   $missi = mysqli_query($mu2,"UPDATE users SET address='".myf1($_POST['address'])."' WHERE id='".myf2($_GET['i'])."'");
                   }
           if(!empty($_POST['section'])){
-              
+
         $m2i = mysqli_query($mu2,"SELECT * FROM sections WHERE name='".myf1($_POST['section'])."'");
               $ssw = mysqli_fetch_assoc($m2i);
               if(!empty($ssw['id'])){
-                            
+
         $missi = mysqli_query($mu2,"UPDATE users SET section='".myf1($ssw['id'])."' WHERE id='".myf2($_GET['i'])."'");
-                  
+
                      if(!empty($_POST['area'])){
-                         
+
                $m52i = mysqli_query($mu2,"SELECT * FROM areas WHERE name='".myf1($_POST['area'])."' AND section_id='".myf1($ssw['id'])."'");
               $ssw2 = mysqli_fetch_assoc($m52i);
-                         
+
               if(!empty($ssw2['id'])){
-                 
-                  
+
+
         $missi = mysqli_query($mu2,"UPDATE users SET area='".myf1($ssw2['id'])."' WHERE id='".myf2($_GET['i'])."'");
                   }else {
                      echo "
@@ -185,7 +185,7 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
               $date_sy = "";
               $nu=0;
                         //2019-03-31
-                      
+
                 while($nu<strlen($_POST['dated'])){
                     if($nu<=3){
                       $date_sy .= $_POST['dated'][$nu];
@@ -198,29 +198,29 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
                 }
                       // date_y='".myf1($date_sy)."',
                         $mi8i = mysqli_query($mu2,"UPDATE el2st SET date_m='".myf1($date_sm)."',date_d='".myf1($date_sd)."' WHERE user_id='".$_GET['i']."'");
-                  
-                  
+
+
                       }
-                  
-                  
-           
-        
-                  
+
+
+
+
+
               }else {
                      echo "
                     <script>alert('لا توجد هذه المنطقة الرئيسية');</script>
                     ";
                       }
                   }
-       
-                               
+
+
                 if(isset($missi)){
                      header("Location: clientpage.php?i=".$_GET['i']);
                 }
-              
+
                   }
-      
-     
+
+
       ?>
         </form>
 </section>
@@ -228,24 +228,24 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
   <div uk-grid>
     <div class="uk-width-1-2">
       <h3>
-         <?php 
+         <?php
           $nummber =0;
           $nummber2 =0;
           $nummber3 =0;
             $misi = mysqli_query($mu2,"SELECT * FROM el2st WHERE user_id='".$_GET['i']."' AND kind=''");
             while($uuis = mysqli_fetch_assoc($misi)){
                 $nummber += intval(myf2($uuis['total']));
-                
+
             }
           $misi = mysqli_query($mu2,"SELECT * FROM el2st WHERE user_id='".$_GET['i']."' AND kind='".myf1('2st_pay')."'");
             while($uuis = mysqli_fetch_assoc($misi)){
                 $nummber2 += intval(myf2($uuis['price']));
-                
+
             }
            $misi = mysqli_query($mu2,"SELECT * FROM el2st WHERE user_id='".$_GET['i']."' AND kind='".myf1('2st_mortag3')."'");
             while($uuis = mysqli_fetch_assoc($misi)){
                 $nummber3 += intval(myf2($uuis['price']));
-                
+
             }
           $ttoal = $nummber - ($nummber2 + $nummber3);
           echo " قسط "." ( ".$ttoal." )";
@@ -263,7 +263,7 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
           </tr>
         </thead>
         <tbody>
-            <?php 
+            <?php
             $nunumb=0;
             $tex="";
             $mii = mysqli_query($mu2,"SELECT * FROM el2st WHERE user_id='".$_GET['i']."' AND kind='' OR user_id='".$_GET['i']."' AND kind='".myf1('2st_pay')."' OR user_id='".$_GET['i']."' AND kind='".myf1('2st_mortag3')."'");
@@ -272,7 +272,7 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
                 if($nunumb==0){
                     $note = myf2($uus['notes']);
                 }
-                
+
                 if($uus['kind']==myf1('2st_mortag3')){
                       $tex=' <td> '.myf2($uus['notes']).' </td>
             <td> مرتجع </td>';
@@ -282,10 +282,10 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
                 }else if($uus['kind']==""){
                     $tex=' <td> '.$note.' </td>
             <td> قسط </td>';
-                } 
+                }
                 $nunumb++;
             echo '
-            
+
           <tr class="table__row">
             <td>'.myf2($uus['date_today']).'</td>
             <td>'.myf2($uus['product_name']).'</td>
@@ -298,17 +298,17 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
             </td>';
                 }else {
                     echo '<td></td>';
-                        
+
                 }
             echo '
-                
+
           </tr>
             ';
             }
             ?>
         </tbody>
       </table>
-        
+
       <div class="" uk-grid>
         <div class="uk-width-1-4">
        <button class="uk-button uk-button-default uk-width-expand" type="button" name="button" href="#new_installment" uk-toggle=""> قسط جديد </button>
@@ -324,35 +324,35 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
         </div>
       </div>
     </div>
-      
-      
-   
-      
-     
+
+
+
+
+
     <div class="uk-width-1-2">
       <h3>
-           <?php 
+           <?php
           $nummbers =0;
             $nummber2 =0;
             $nummber3 =0;
     $misi = mysqli_query($mu2,"SELECT * FROM el2st WHERE user_id='".$_GET['i']."' AND kind='".myf1('cash')."'");
             while($uuis = mysqli_fetch_assoc($misi)){
                 $nummbers += intval(myf2($uuis['total']));
-                
+
             }
-       
+
              $misi = mysqli_query($mu2,"SELECT * FROM el2st WHERE user_id='".$_GET['i']."' AND kind='".myf1('cash_pay')."'");
             while($uuis = mysqli_fetch_assoc($misi)){
                 $nummber2 += intval(myf2($uuis['price']));
-                
+
             }
-          
+
             $misi = mysqli_query($mu2,"SELECT * FROM el2st WHERE user_id='".$_GET['i']."' AND kind='".myf1('cash_mortag3')."'");
             while($uuis = mysqli_fetch_assoc($misi)){
                 $nummber3 += intval(myf2($uuis['price']));
-                
+
             }
-          
+
           $tttoal = $nummbers - ($nummber2 + $nummber3);
           echo " كاش "." ( ".$tttoal." )";
           ?>
@@ -369,22 +369,22 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
           </tr>
         </thead>
         <tbody>
-               <?php 
+               <?php
             $tex="";
             $miis = mysqli_query($mu2,"SELECT * FROM el2st WHERE user_id='".$_GET['i']."' AND kind='".myf1('cash_mortag3')."' OR user_id='".$_GET['i']."' AND kind='".myf1('cash_pay')."' OR user_id='".$_GET['i']."' AND kind='".myf1('cash')."'");
             while($uusw = mysqli_fetch_assoc($miis)){
                 $totall =  (intval(myf2($uusw['price'])) * intval(myf2($uusw['useful'])) / 100) + intval(myf2($uusw['price']));
-                
+
                  if($uusw['kind']==myf1('cash_mortag3')){
                     $tex="مرتجع";
                 }else if($uusw['kind']==myf1('cash_pay')){
                     $tex="دفع كاش";
                 }else if($uusw['kind']==myf1('cash')){
                     $tex="كاش";
-                } 
-                
+                }
+
             echo '
-            
+
           <tr class="table__row">
             <td>'.myf2($uusw['date_today']).'</td>
             <td>'.myf2($uusw['product_name']).'</td>
@@ -398,14 +398,14 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
             </td>';
                 }else {
                     echo '<td></td>';
-                        
+
                 }
           echo '</tr>
             ';
             }
             ?>
-            
-     
+
+
         </tbody>
       </table>
       <div class="" uk-grid>
@@ -425,9 +425,9 @@ $mss = mysqli_query($mu2,"SELECT * FROM users WHERE id='".myf2($_GET['i'])."'");
     </div>
 
 
-    
+
         </div>
-      
-    
+
+
 </section>
 <?php include'footer.php'; ?>
