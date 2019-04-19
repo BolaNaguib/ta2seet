5381ee25b758b61291c2cc3b7cc99fdb6d7840fe
Bola Naguib <bolanaguib@gmail.com>
2019-04-19 03:40:13 +0200

    added district style

--- district.php
+++ district.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 ob_start();
 include'header.php';
@@ -16,13 +16,13 @@ if(empty($_SESSION['didi'])){
       if(empty($_SESSION['ta2_admin'])){
          $hus = mysqli_query($mu2,"SELECT * FROM persmission WHERE worker_id='".$_SESSION['didi']."'");
       while($uhs=mysqli_fetch_assoc($hus)){
-          
+
       $hu = mysqli_query($mu2,"SELECT * FROM sections WHERE id='".myf2($uhs['section_id'])."'");
       $uh=mysqli_fetch_assoc($hu);
           $name = myf2($uh['name']);
           echo '
           <div class="uk-width-1-3 uk-first-column">
-      <a href="area.php?n='.$uh['name'].'&i='.myf1($uh['id']).'">  <div class="uk-card uk-card-default uk-padding uk-padding-large">
+      <a href="area.php?n='.$uh['name'].'&i='.myf1($uh['id']).'">  <div class="uk-card uk-card-default uk-padding uk-padding-large districtcard">
 
                   <h1> '.$name.' </h1>
 
@@ -30,9 +30,9 @@ if(empty($_SESSION['didi'])){
       </div></a>
     </div>
           ';
-      } 
+      }
       }else {
- 
+
       $hu = mysqli_query($mu2,"SELECT * FROM sections");
       while($uh=mysqli_fetch_assoc($hu)){
           $name = myf2($uh['name']);
@@ -46,16 +46,16 @@ if(empty($_SESSION['didi'])){
       </div></a>
     </div>
           ';
-      
+
       }
       }
-    
+
       if(!empty($_SESSION['ta2_admin'])){
-    
+
           echo '
-    
+
         <div class="uk-width-1-3">
-      <a href="#new_category" uk-toggle> 
+      <a href="#new_category" uk-toggle>
 
         <div class="uk-card uk-card-default uk-padding uk-padding-large">
           <h1>
@@ -63,10 +63,10 @@ if(empty($_SESSION['didi'])){
           </h1>
         </div>
       </a>
-      
+
           ';
       }
-      
+
       ?>
   </div>
 </section>
