f1c4dce7fa77818935e3cf36832c1971660cceb2
Bola Naguib <bolanaguib@gmail.com>
2019-05-17 23:25:59 +0200

    updated db

---
+++ connect/confi.php
@@ -0,0 +1,11 @@
+<?php
+
+$server_name = "localhost";
+$user_name = "arabswar_tasss";
+$pass = "}tOt*0jHpP#W";
+$db_name = "arabswar_tasset";
+
+$mu2 = @mysqli_connect($server_name,$user_name,$pass,$db_name);
+
+mysqli_set_charset($mu2,'utf8');
+?>
